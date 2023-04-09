4fefc9b582b825fa7728379b672316664d622b47
GabbiReis <gabrielastefanereis@gmail.com>
2023-04-09 19:52:56 -0300

    Atividades H1

---
+++ Missão 4/Program.cs
@@ -0,0 +1,4 @@
+﻿
+Console.WriteLine("Resposta:");
+Console.WriteLine("Está falando a verdade, se utilizarmos o algoritmo de ordenação Merge Sort, é possível fazer 6 comparações para ordenar uma sequencia de números, visto que esse algoritmo divide um numero em partes menores, que nesse caso seria 6 log 6");
+
